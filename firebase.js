39d9c916572280c52b0aa0e6526806bd8b5cc609
jamesv491 <jamesviola491@gmail.com>
2026-05-24 12:40:46 -0700

    Add Firebase configuration and setup

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZlow4V5iVLHGuRamztoZQXVSbp9tGSOs",
+  authDomain: "securoutine.firebaseapp.com",
+  projectId: "securoutine",
+  storageBucket: "securoutine.firebasestorage.app",
+  messagingSenderId: "994435974311",
+  appId: "1:994435974311:web:76ae0c4560a8f098050782"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
